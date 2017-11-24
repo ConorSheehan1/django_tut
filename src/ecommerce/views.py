98ff33424bc84fa47617fee02a92644e2f85f379
conor <conor.sheehan@live.ie>
2017-11-24 15:14:45 +0000

    add template

--- src/ecommerce/views.py
+++ src/ecommerce/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home_page(reguest):
-    return HttpResponse("<h1>Hello world</h1>")
+    return render(reguest, "home_page.html", {})
