a79d58c1d3470722759c9e06ef0202da63a27137
conor <conor.sheehan@live.ie>
2017-11-24 15:57:32 +0000

    start form

--- src/ecommerce/views.py
+++ src/ecommerce/views.py
@@ -1,5 +1,23 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def home_page(reguest):
-    return render(reguest, "home_page.html", {})
+def home_page(request):
+    context = {
+        'title':'Home',
+        'content': 'Welcome to the home page'
+    }
+    return render(request, "home_page.html", context)
+
+def about_page(request):
+    context = {
+        'title':'About'
+    }
+    return render(request, "home_page.html", context)
+
+def contact_page(request):
+    context = {
+        'title':'Contact'
+    }
+    if request.method == 'POST':
+        print(request.POST)
+    return render(request, "contact/view.html", context)
